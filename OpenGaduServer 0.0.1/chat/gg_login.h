cf7e3eaf39afdc2286df2aafbe024babce8aca8d
Jakub Kretkowski <kreciak09+github@gmail.com>
2026-03-17 10:26:14 +0100

    add more comments

--- OpenGaduServer 0.0.1/chat/gg_login.h
+++ OpenGaduServer 0.0.1/chat/gg_login.h
@@ -6,18 +6,17 @@
 
 typedef struct {
 	uint32_t type;		// for example GG_WELCOME
-	uint32_t length;	// sizeof(struct)
+	uint32_t length;	// sizeof(struct), or 0 if just header
 } GG_PACKED GG_header ;
 
-#define GG_WELCOME	0x00000001
-
+#define GG_WELCOME	0x0001
 
 typedef struct {
 	int seed;	// its a 32-bit random number / change to uint32_t
 } GG_PACKED GG_welcome;
 
-#define GG_LOGIN3 0x00000001 // yes, it is GG_WELCOME
-#define GG_LOGIN 0x000c		// struct: libgadu/include/libgadu.h.in @ 1888
+#define GG_LOGIN3 0x00000001 	// yes, it is GG_WELCOME / todo: reverse engineer the struct for v3
+#define GG_LOGIN 0x000c			// libgadu/include/libgadu.h.in @ 1888 - GG_LOGIN struct
 
 // Acording to wireshark, the client sends out 
 // a packet of this type, but its 12 bytes in length.
@@ -33,7 +32,7 @@ typedef struct {
 // ----- functions -----------------
 
 void gg_send_welcome(int sock, uint32_t seed);
-uint32_t gg_login_hash_old(const unsigned char *password, uint32_t seed);
+uint32_t gg_login_hash_old(const unsigned char *password, uint32_t seed);	// pls get rid of this
 void gg_send_login_failed(int sock);
 void gg_send_login_ok(int sock);
 void handle_logging(int sock);
